4613b3f0675b9fc846dc3edc890e4bbf6db9f88a
@S52000216 <52000216@student.tdtu.edu.vn>
2025-04-29 10:27:28 +0700

    feat: add link typed to backend

--- routes/api/links.js
+++ routes/api/links.js
@@ -178,7 +178,7 @@ router.put("/:linkId", authMiddleware, async (req, res) => {
   try {
     const updatedLink = await Link.findOneAndUpdate(
       { _id: linkId, userId: req.user.userId }, // Đảm bảo user chỉ update link của mình
-      { $set: { title, url } },
+      { $set: updateFields },
       { new: true, runValidators: true }
     );
 
